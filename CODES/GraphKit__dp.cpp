2962944870d792a76db97123ba0e217bd8b3da0f
HuRuilizhen <122090168@link.cuhk.edu.cn>
2024-02-16 23:54:44 +0800

    update multi-thread dp

--- CODES/GraphKit__dp.cpp
+++ CODES/GraphKit__dp.cpp
@@ -15,7 +15,10 @@ namespace GraphKitDp
 {
     int numThreadsAlive = 0;
     std::mutex mutex;
+    std::mutex memoizationMutex;
+    std::mutex numThreadsAliveMutex;
     std::condition_variable conditionVariable;
+
     struct Memoization
     {
         std::vector<int> schedule;
@@ -24,34 +27,60 @@ namespace GraphKitDp
         int peak_memory;
     };
 
-    void workThread(int node, std::set<int> newCandidate, std::vector<int> newSchedule, std::vector<int> newIndegree, std::map<std::set<int>, Memoization> &currentMemoization, int currentMemory, int peakMemory, Graph graph, int *inSum, int *outSum)
+    void numThreadsAliveIncrement()
     {
+        std::lock_guard<std::mutex> lock(numThreadsAliveMutex);
         numThreadsAlive++;
-        // std::unique_lock<std::mutex> lock(mutex);
-        // lock.unlock();
+    }
 
-        newCandidate.erase(node);
-        newSchedule.push_back(node);
+    void numThreadsAliveDescrement()
+    {
+        std::lock_guard<std::mutex> lock(numThreadsAliveMutex);
+        numThreadsAlive--;
+    }
 
-        for (int edge = graph.getEdgeHead(node); graph.isValid(edge); edge = graph.getEdgeNext(edge))
+    bool numThreadsAliveChecker()
+    {
+        std::lock_guard<std::mutex> lock(numThreadsAliveMutex);
+        return !numThreadsAlive;
+    }
+
+    void workThread(std::set<int> currentCandidate, std::vector<int> currentSchedule, std::vector<int> currentIndegree, std::map<std::set<int>, Memoization> &currentMemoization, int currentMemory, int peakMemory, Graph graph, int *inSum, int *outSum)
+    {
+        numThreadsAliveIncrement();
+
+        for (auto node : currentCandidate)
         {
-            int to = graph.getEdgeTo(edge);
-            if (--newIndegree[to])
-                continue;
-            newCandidate.insert(to);
+            std::set<int> newCandidate = currentCandidate;
+            std::vector<int> newSchedule = currentSchedule;
+            std::vector<int> newIndegree = currentIndegree;
+
+            newCandidate.erase(node);
+            newSchedule.push_back(node);
+
+            for (int edge = graph.getEdgeHead(node); graph.isValid(edge); edge = graph.getEdgeNext(edge))
+            {
+                int to = graph.getEdgeTo(edge);
+                if (--newIndegree[to])
+                    continue;
+                newCandidate.insert(to);
+            }
+
+            int newCurrentMemory = currentMemory - inSum[node] + outSum[node];
+            int newPeakMemory = std::max(peakMemory, newCurrentMemory);
+
+            std::unique_lock<std::mutex> memoizationLock(memoizationMutex);
+            if (currentMemoization.find(newCandidate) == currentMemoization.end())
+                currentMemoization.emplace(newCandidate, GraphKitDp::Memoization{newSchedule, newIndegree, newCurrentMemory, newPeakMemory});
+            else if (currentMemoization.at(newCandidate).peak_memory > newPeakMemory)
+                currentMemoization.find(newCandidate)
+                    ->second = GraphKitDp::Memoization{newSchedule, newIndegree, newCurrentMemory, newPeakMemory};
+            memoizationLock.unlock();
         }
 
-        int newCurrentMemory = currentMemory - inSum[node] + outSum[node];
-        int newPeakMemory = std::max(peakMemory, newCurrentMemory);
-
-        if (currentMemoization.find(newCandidate) == currentMemoization.end())
-            currentMemoization.emplace(newCandidate, GraphKitDp::Memoization{newSchedule, newIndegree, newCurrentMemory, newPeakMemory});
-        else if (currentMemoization.at(newCandidate).peak_memory > newPeakMemory)
-            currentMemoization.find(newCandidate)->second = GraphKitDp::Memoization{newSchedule, newIndegree, newCurrentMemory, newPeakMemory};
-
-        // lock.lock();
-        numThreadsAlive--;
-        // conditionVariable.notify_all();
+        numThreadsAliveDescrement();
+        if (numThreadsAliveChecker())
+            conditionVariable.notify_all();
     }
 };
 
@@ -80,7 +109,9 @@ void GraphKit::dp(int &currentMemory, int &peakMemory)
     */
     for (int i = 0; i < graph.getNumNodes(); i++)
     {
+        std::vector<std::thread> threads;
         std::map<std::set<int>, GraphKitDp::Memoization>::iterator it;
+
         for (it = lastMemoization.begin(); it != lastMemoization.end(); it++)
         {
             std::set<int> currentCandidate = it->first;
@@ -90,19 +121,16 @@ void GraphKit::dp(int &currentMemory, int &peakMemory)
             int currentMemory = memoization.current_memory;
             int peakMemory = memoization.peak_memory;
 
-            std::vector<std::thread> threads;
-            for (auto node : currentCandidate)
-                GraphKitDp::workThread(node, currentCandidate, currentSchedule, currentIndegree, currentMemoization, currentMemory, peakMemory, graph, inSum, outSum);
-
-            // std::unique_lock<std::mutex> lock(GraphKitDp::mutex);
-            // while (GraphKitDp::numThreadsAlive > 0)
-            //     GraphKitDp::conditionVariable.wait(lock);
-            // lock.unlock();
-
-            // for (auto &thread : threads)
-            //     if (thread.joinable())
-            //         thread.join();
+            threads.emplace_back(GraphKitDp::workThread, currentCandidate, currentSchedule, currentIndegree, std::ref(currentMemoization), currentMemory, peakMemory, graph, inSum, outSum);
         }
+
+        std::unique_lock<std::mutex> lock(GraphKitDp::mutex);
+        GraphKitDp::conditionVariable.wait(lock);
+
+        for (auto &thread : threads)
+            if (thread.joinable())
+                thread.join();
+
         lastMemoization = currentMemoization;
         currentMemoization.clear();
     }
